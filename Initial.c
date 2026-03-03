cbe91fb8784147040d6ee15f42e17df6200ba2a2
WildCraftsmanFilter <rudnevairina799@gmail.com>
2026-03-03 17:21:30 -0300

    Initial commit

--- Initial.c
+++ Initial.c
@@ -1,11 +1,3 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * @brief Performs basic arithmetic operation based on operator and operands.
- * @param a First operand (double)
- * @param b Second operand (double)
- * @param op Operator character (+, -, *, /)
- * @return Result of the operation as double
- * @note Returns 0.0 and prints error message for invalid operations
- */
